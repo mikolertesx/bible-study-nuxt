bfebe4a9d79e663d42461f8d700e172de7fe57c0
Mike <mgangsal@outlook.com>
2021-01-03 20:12:52 -0600

    Added preliminary tools to the page

--- components/read/tools.vue
+++ components/read/tools.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="w-full">
     <p class="text-center mt-8">Tools</p>
-    <div v-if="selected.length === 0">
+    <div v-if="selectedVerses.length === 0">
       <p class="text-gray-600 text-center">No verse has been selected.</p>
     </div>
-    <div v-else-if="selected.length === 1">
-      <p class="text-center">Verse {{ selected }} selected.</p>
+    <div v-else-if="selectedVerses.length === 1">
+      <p class="text-center text-gray-600">One verse selected</p>
+      <p>{{ selectedVerses[0].text }}</p>
     </div>
     <div v-else>
-      <p class="text-center">Multiple verses were selected.</p>
+      <p class="text-center text-gray-600">Multiple verses were selected.</p>
+      <p v-for="verse in selectedVerses" :key="verse.id">{{ verse.text }}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    selected: {
-      type: Array,
-      required: false,
-      default: () => [],
+  computed: {
+    selectedVerses() {
+      return this.$store.getters['read/selectedVerses']
     },
   },
 }
