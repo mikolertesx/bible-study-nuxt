df25f5d519ddd2e1579cffb4bf95e89ac11386e8
Mike <mgangsal@outlook.com>
2021-02-05 18:44:55 -0600

    Added a mobile header

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 ## Uses
 An app to better organize bible readings in group.
 ## TODO
-- Maybe add a notes state (So that the state with the verses is more concise).
 - Finish adding the "add-notes" functionality into the server.
--- components/layout/Headers/ReadHeader.vue
+++ components/layout/Headers/ReadHeader.vue
@@ -1,23 +1,35 @@
 <template>
-  <header class="w-full p-2 h-16 flex-none">
-    <nav class="flex justify-between box-border py-1 px-4 item-center h-full">
-      <select v-model="selectedBook">
+  <header class="w-full p-2 h-auto md:h-16 flex-none">
+    <nav
+      class="flex flex-col md:flex-row justify-around md:justify-between items-center md:items-stretch box-border space-y-4 md:space-y-0 py-1 px-4 item-center w-full h-full"
+    >
+      <select v-model="selectedBook" class="w-full md:w-auto h-8 my-auto">
         <option v-for="option in options" :key="option.id" :value="option.name">
           {{ option.name }}
         </option>
       </select>
-      <div>
-        <app-button :enabled="!isFirstChapter" @click="goPrev">Back</app-button>
+      <div class="flex md:block w-full md:w-auto justify-around">
+        <app-button :enabled="!isFirstChapter" @click="goPrev">
+          <p class="hidden md:inline">Back</p>
+          <SolidChevronLeftIcon
+            class="inline md:hidden h-8"
+          ></SolidChevronLeftIcon>
+        </app-button>
         <select
           v-if="chapters !== null"
           v-model="selectedChapter"
-          class="h-full"
+          class="h-full flex-grow mx-4"
         >
           <option v-for="chapter in chapters" :key="chapter" :value="chapter">
             {{ chapter }}
           </option>
         </select>
-        <app-button :enabled="!isLastChapter" @click="goNext">Next</app-button>
+        <app-button :enabled="!isLastChapter" @click="goNext">
+          <p class="hidden md:inline">Next</p>
+          <SolidChevronRightIcon
+            class="inline md:hidden h-8"
+          ></SolidChevronRightIcon>
+        </app-button>
       </div>
     </nav>
   </header>
